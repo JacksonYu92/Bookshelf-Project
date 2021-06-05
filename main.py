803aa68c56a31dc2f83d335ee5c376cb45542b24
JacksonYu92 <64330362+JacksonYu92@users.noreply.github.com>
2021-06-05 06:03:30 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////udemy/100days of Python/2nd/day-63/library-start/books-collection.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float(), nullable=False)
+
+    #Optional: this will allow each book object to be identified by its title when printed.
+    def __repr__(self):
+        return f'<Book {self.title}>'
+db.create_all()
+
+@app.route('/')
+def home():
+    all_books = db.session.query(Book).all()
+    return render_template('index.html', books=all_books)
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if request.method == "POST":
+        new_book = Book(
+            title=request.form["title"],
+            author=request.form["author"],
+            rating=request.form["rating"],
+        )
+        db.session.add(new_book)
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template('add.html')
+
+@app.route("/edit/<int:id>", methods=["GET", "POST"])
+def edit(id):
+    all_books = db.session.query(Book).all()
+    if request.method == "POST":
+        book_to_update = Book.query.get(id)
+        book_to_update.rating = request.form["rating"]
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template('edit.html', books=all_books)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
